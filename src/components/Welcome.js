7148ca62c8a5a160b815889b3c6c3d40ef37e0b3
JONI <arjoni95@gmail.com>
2020-11-02 00:06:14 -0800

    condition

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,12 +4,13 @@ class Welcome extends Component{
     constructor() {
         super()
         this.state = {
-            name:"Abdur Rahim",
+            /*name:"Abdur Rahim",
             age:["28","30"],
             weight:{
                 class10:"20kg",
                 class12:"40kg"
-            }
+            }*/
+            login:false
         }
     }
     doThisClass(a){
@@ -19,13 +20,22 @@ class Welcome extends Component{
         this.setState({age:b})
     }
     render() {
-        return <div>
-            <h3>My name is {this.state.name}</h3>
+        /*if (this.state.login == true){
+            return <div>
+                <button>Logout</button>
+            </div>;
+        }
+        else {
+            return <div>
+                <h3>My name is {this.state.name}</h3>
             <h3>My age is {this.state.age[1]}</h3>
             <h4>My weight {this.state.weight.class10}</h4>
             <button class="btn btn-success btn-sm" onClick={this.doThisClass.bind(this, "Joni")}>Change Name</button>
             <button class="btn btn-success btn-sm" onClick={this.doThisChangeClass.bind(this, "23")}>Change Age</button>
-        </div>;
+                <button>Login</button>
+            </div>;
+        }*/
+        return this.state.login?'Logout':'Login'
     }
 }
 export default Welcome;
