0a3903468f10ab2cdd2b7638ebb94e8bb077f022
JONI <arjoni95@gmail.com>
2020-11-03 00:48:35 -0800

    form

--- src/components/NewDemo.js
+++ src/components/NewDemo.js
@@ -6,7 +6,8 @@ class NewDemo extends Component {
         super();
         this.state={
             name:"",
-            mobile:""
+            mobile:"",
+            description:"",
         }
     }
     changeValue=(event)=>{
@@ -33,7 +34,18 @@ class NewDemo extends Component {
                 <form>
                     <h3>Input data</h3>
                     <input type="text" name="name" onChange={this.changeValue}></input>
-                    <input type="mobile" name="mobile" onChange={this.changeValue}></input>
+                    <input type="mobile" name="mobile" onChange={this.changeValue}></input><br></br>
+                    <textarea name="description" onChange={this.changeValue} placeholder="Write here"></textarea>
+                    <br></br>
+                    <br></br>
+                    <select>
+                        <option>One</option>
+                        <option>Two</option>
+                        <option>Three</option>
+                        <option>Four</option>
+                    </select>
+                    <br></br>
+                    <br></br>
                     <input type="submit"></input>
                 </form>
                 <hr></hr>
@@ -41,6 +53,7 @@ class NewDemo extends Component {
                 <h3>Output data</h3>
         <p>{this.state.name}</p>
         <p>{this.state.mobile}</p>
+        <p>{this.state.description}</p>
             </div>
         );
     }
